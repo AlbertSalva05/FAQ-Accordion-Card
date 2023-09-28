64ab535a9fde21a9f2852daa35bddbd0f9a6e468
AlbertSalva05 <albertsalva360@gmail.com>
2023-09-28 10:59:10 +0800

    update 230928_02

--- root/js/main.js
+++ root/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
-	$(".faqQuestion").click(function(){
+    // onclick function on faqQuestion
+    $(".faqQuestion").click(function(){
+        // toggle class active on trigger
         $(this).toggleClass("faq_acc_active");
+        // displaying of faqPanel
         $(this).next().slideToggle(300);
     });
 });
